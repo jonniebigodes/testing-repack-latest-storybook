a608023de592eaec7274b30cedcb45e9490f7ed2
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2026-05-14 20:56:29 +0100

    Additional Chromatic configuration

--- metro.config.js
+++ metro.config.js
@@ -11,8 +11,8 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
-  enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true",
-  //enabled: true, // Required for Chromatic, since it doesn't set the environment variable when building the app
+  //enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true",
+  enabled: true, // Required for Chromatic, since it doesn't set the environment variable when building the app
 };
 
 module.exports = withStorybook(mergeConfig(getDefaultConfig(__dirname), config));
